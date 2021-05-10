c26789bd379e0a83562bc694b7850258d453db69
Haahr2111 <36225747+Haahr2111@users.noreply.github.com>
2021-05-10 19:33:00 +0200

    done

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-window.onscroll = function() {myFunction()};
+var slideIndex = [1,1];
+var slideId = ["slide_1", "slide_2", "slide_3", "slide_4"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
 }
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 2) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+
